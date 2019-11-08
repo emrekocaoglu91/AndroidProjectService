283396c6d3645187704553fcdebd0d73f6c5af83
emrekocaoglu91 <57230080+emrekocaoglu91@users.noreply.github.com>
2019-11-08 13:36:14 +0300

    Repo ve service değişiklikleri.

--- src/main/java/com/deneme/Korku/Hikayeleri/repository/UserRepository.java
+++ src/main/java/com/deneme/Korku/Hikayeleri/repository/UserRepository.java
@@ -7,8 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
 
-
-
+    UserEntity findByEmail(String email);
 
 
 }
--- src/main/java/com/deneme/Korku/Hikayeleri/service/UserServiceImpl.java
+++ src/main/java/com/deneme/Korku/Hikayeleri/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.deneme.Korku.Hikayeleri.service;
 import com.deneme.Korku.Hikayeleri.entity.UserEntity;
 import com.deneme.Korku.Hikayeleri.repository.UserRepository;
 import com.deneme.Korku.Hikayeleri.shared.dto.UserDto;
+import com.deneme.Korku.Hikayeleri.shared.dto.Utils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,21 +14,32 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    Utils utils;
+
     @Override
     public UserDto createUser(UserDto userDto) {
+
+        // Aynı kontrolü userName için de yapmalıyız.
+        if (userRepository.findByEmail(userDto.getEmail()) != null)
+            throw new RuntimeException("Record already exists.");
+
         UserEntity userEntity = new UserEntity();
 
         BeanUtils.copyProperties(userDto, userEntity);
 
+
+        //İstediğimiz uzunlukta random userId oluşturmak için
+
+        String publicUserId = utils.generateUserId(30);
+        userEntity.setUserId(publicUserId);
+
         userEntity.setEncryptedPassword("test");
-        userEntity.setUserId("testUserId");
-
-
         UserEntity storedUserEntity = userRepository.save(userEntity);
 
         UserDto dto = new UserDto();
 
-        BeanUtils.copyProperties(storedUserEntity,dto);
+        BeanUtils.copyProperties(storedUserEntity, dto);
 
         return dto;
     }
